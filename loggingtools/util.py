41bcf7c3e5fa914d5f8a36af59dc2823ee7a9f6e
tolland1 <jaan.tollander@gmail.com>
2017-03-14 18:22:59 +0200

    templates, yaml_to_json

---
+++ loggingtools/util.py
@@ -0,0 +1,12 @@
+import yaml
+import json
+import os
+
+
+def convert_yaml_to_json(filepath='logging.yaml'):
+    with open(filepath, 'r') as fp:
+        config = yaml.load(fp)
+
+    base, _ = os.path.splitext(filepath)
+    with open(base + '.json', 'w') as fp:
+        json.dump(config, fp, indent=2)
--- templates/yaml_to_json.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import json
-
-with open('logging.yaml', 'r') as fp:
-    config = yaml.load(fp)
-
-with open('logging.json', 'w') as fp:
-    json.dump(config, fp, indent=2)
