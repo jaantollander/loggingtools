e14a9eeb0d1dc604f36fe11b22049dcea5114383
tolland1 <jaan.tollander@gmail.com>
2017-03-19 11:35:08 +0200

    travis setup files

--- loggingtools/__init__.py
+++ loggingtools/__init__.py
@@ -2,7 +2,10 @@ from loggingtools.setup_logging import setup_logging
 from loggingtools.log_with import log_with
 
 __author__ = 'Jaan Tollander de Balsch'
+__github_repo__ = 'jaantollander/loggingtools'
+
 __version__ = '0.1.0'
+
 __all__ = """
 setup_logging
 log_with
