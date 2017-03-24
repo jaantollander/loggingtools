fef4e705a85b4fd998ce292142035812b4aa6013
jaan <jaan.tollander@gmail.com>
2017-03-24 20:04:44 +0200

    versioneer, continuous integration

--- loggingtools/__init__.py
+++ loggingtools/__init__.py
@@ -1,10 +1,13 @@
 from loggingtools.setup_logging import setup_logging
 from loggingtools.log_with import log_with
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
 __author__ = 'Jaan Tollander de Balsch'
 __github_repo__ = 'jaantollander/loggingtools'
-
-__version__ = '0.1.0'
 
 __all__ = """
 setup_logging
