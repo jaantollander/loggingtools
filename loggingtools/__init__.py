4b0170c6b3aa1a8f7a8e2eaf2f5bc539154ac54f
jaan <jaan.tollander@gmail.com>
2017-03-27 12:14:09 +0300

    setup.py fixes

--- loggingtools/__init__.py
+++ loggingtools/__init__.py
@@ -1,15 +1,16 @@
-from loggingtools.setup_logging import setup_logging
 from loggingtools.log_with import log_with
-
+from loggingtools.setup_logging import setup_logging
 from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
-
-__author__ = 'Jaan Tollander de Balsch'
-__github_repo__ = 'jaantollander/loggingtools'
 
 __all__ = """
 setup_logging
 log_with
 """.split()
+
+__author__ = 'Jaan Tollander de Balsch'
+__email__ = 'jaan.tollander@gmail.com'
+__version__ = get_versions()['version']
+__github_repo__ = 'jaantollander/loggingtools'
+__license__ = 'MIT'
+
+del get_versions
--- setup.py
+++ setup.py
@@ -1,20 +1,37 @@
 from setuptools import setup
-from loggingtools import __version__
+from loggingtools import __version__, __license__, __author__, __email__
 
+
+install_requires = [
+    'colorlog',
+    'colorama',
+    'ruamel.yaml',
+    'click',
+]
+tests_require = [
+    'tox',
+    'pytest',
+    'pytest-cov',
+    'pytest-xdist',
+]
 
 setup(
     name='loggingtools',
     version=__version__,
     packages=['loggingtools', 'loggingtools.tests'],
+    include_package_data=True,
     url='https://github.com/jaantollander/loggingtools.git',
-    license='MIT',
-    author='Jaan Tollander de Balsch',
-    author_email='jaan.tollander@gmail.com',
-    description='Python logging dictionary configuration from yaml, json or Python dict',
+    license=__license__,
+    author=__author__,
+    author_email=__email__,
+    description='Python logging dictionary configuration from yaml, json or '
+                'dictionary.',
     entry_points={
         'console_scripts': [
             'loggingtools=loggingtools.cli:cli',
         ]
     },
-    install_requires=['typing', 'colorlog', 'colorama', 'ruamel.yaml', 'click']
+    install_requires=install_requires,
+    test_suite='tests',
+    tests_require=tests_require,
 )
